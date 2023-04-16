e7ff794ce9756804d93b32aa95cca09a5c9404ff
Chen <129372913+Chen08-16@users.noreply.github.com>
2023-04-16 16:11:46 +0800

    Update advice.py

--- advice.py
+++ advice.py
@@ -2,15 +2,13 @@ import pandas as pd
 
 def get_good_practice(egg_type):
 
-    df = pd.read_csv('Eggs1.csv')
+    df = pd.read_csv('Eggs.csv')
     mask = df['Egg_Type'] == egg_type.capitalize()
     good_practice = df.loc[mask, 'Good_Practice'].values
     if len(good_practice) == 0:
         return None
     else:
         return good_practice[0]
-
-
 
 #Use the function above
 good_practice = get_good_practice('cHicken')
